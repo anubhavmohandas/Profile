799fbccc74b964f743556fc1c82cb3e8526e593d
anubhavmohandas <anubhav.manav147@gmail.com>
2022-04-02 01:21:21 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import About from "./Components/About/about";
 import Intro from "./Components/Intro/intro";
+import Project from "./Components/Project/project";
+import Testimonial from "./Components/Testimonials/Testimonial"
 
 const App = () => {
   return (
       <div>
         <Intro/>
         <About/>
+        <Project/>
+        <Testimonial/>
       </div>
     )
 };
--- src/Components/Project/project.jsx
+++ src/Components/Project/project.jsx
@@ -1,7 +1,15 @@
-const About = () => { 
+import "./project.css"
+
+const Project = () => { 
     return (
-           <div>
+           <div className="p1">
+               <div className="p1-texts">
+                   <h1 className="p1-heading">My Creations</h1>
+                   <p className="p1-description">These are the websites (Projects) made by ME </p>
+               </div>
+               <div className="p1-lists">
+               </div>
            </div>
     )
 }
-export default About
+export default Project
