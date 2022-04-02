62fe2ea5028a24d35d8616e9cbb2e531f8af6cbd
anubhavmohandas <anubhav.manav147@gmail.com>
2022-04-03 02:33:31 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
 import About from "./Components/About/about";
 import Intro from "./Components/Intro/intro";
 import Work from "./Components/work/Work";
 import Contact from "./Components/Contact/Contact";
+import Toggle from "./Components/toggle/Toggle";
+import { ThemeContext } from "./context";
 
 const App = () => {
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-      <div>
+      <div style = {{backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white" }}>
+        <Toggle />
         <Intro/>
         <About/>
         <Work/>
